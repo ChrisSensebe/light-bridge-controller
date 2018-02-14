422203e926e72635a41de63ff13816c583b284c4
ChrisSensebe <christophe.sensebe@gmail.com>
2018-02-14 23:53:57 +0100

    adding app

--- src/server.ts
+++
@@ -1,5 +0,0 @@
-const http = require('http');
-const port = process.env.port || 8080;
-
-const server = http.createServer((req, res) => res.end('hello world'));
-server.listen(port, err => err ? console.log(`Oups, ${err}`) : console.log(`server listening on ${port}`));
