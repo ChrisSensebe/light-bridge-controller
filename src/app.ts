8fe5725538aa76ca6be00c88381ae5553032ed3f
ChrisSensebe <christophe.sensebe@gmail.com>
2018-12-08 21:19:07 +0100

    removing light-bridge-controller and creating bridge.service

--- src/app.ts
+++ src/app.ts
@@ -5,12 +5,10 @@ if (process.argv.length < 4) {
     process.exit(1);
 }
 
-// TODO check url and user
 const bridgeAddress = process.argv[2];
 const bridgeUser = process.argv[3];
 
-const lightController = new BridgeService(bridgeAddress, bridgeUser);
-lightController.watchLightBridge();
+const bridgeService = new BridgeService(bridgeAddress, bridgeUser);
 
 console.log(`Watching hue bridge at ${bridgeAddress} with user ${bridgeUser}`);
 
