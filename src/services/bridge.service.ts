84319208425efa7cbe36f39033887b7bf3b10c4e
ChrisSensebe <christophe.sensebe@gmail.com>
2018-12-08 23:15:17 +0100

    adding methods getLights and getLight to BridgeService

--- src/services/bridge.service.ts
+++ src/services/bridge.service.ts
@@ -1,5 +1,6 @@
 import * as axios from 'axios';
 import {State} from '../models/state.interface';
+import {Light} from '../models/light.interface';
 
 export class BridgeService {
 
@@ -9,6 +10,16 @@ export class BridgeService {
   constructor(bridgeUrl: string, bridgeUser: string) {
     this.bridgeAddress = bridgeUrl;
     this.bridgeUser = bridgeUser;
+  }
+
+  getLights(): Light[] {
+    // TODO
+    return [];
+  }
+
+  getLight(lightId: string): Light {
+    // TODO
+    return undefined;
   }
 
   setLightState(lightId: string, state: State) {
