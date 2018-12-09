750aff60c41539dc07c68eccea2d9500fb845ab1
ChrisSensebe <christophe.sensebe@gmail.com>
2018-12-09 21:50:20 +0100

    adding lights.controller

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,10 +1,12 @@
 import * as express from 'express';
 import {Router} from 'express';
+import {LightsController} from '../controller/lights.controller';
 
 export class Routes {
 
   public static getRoutes(): Router {
     const router = express.Router();
+    router.get('/', LightsController.getRoot);
     return router;
   }
 }
