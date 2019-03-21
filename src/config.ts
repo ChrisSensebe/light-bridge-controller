81d110ead5a8053e596387917595649374276ebb
ChrisSensebe <christophe.sensebe@gmail.com>
2019-03-21 23:11:27 +0100

    adding module for env variables

---
+++ src/config.ts
@@ -0,0 +1,8 @@
+module.exports = {
+  port: process.env.PORT,
+  databaseUri: process.env.DATABASE_URI,
+  databaseUser: process.env.DATABASE_USER,
+  databasePwd: process.env.DATABASE_PASSWORD,
+  bridgeUri: process.env.BRIDGE_URI,
+  bridgeUser: process.env.BRIDGE_USER,
+};
