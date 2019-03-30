a31d25f86b001d24c9ea920d49b0f483c1c2c00f
ChrisSensebe <christophe.sensebe@gmail.com>
2019-03-31 00:03:33 +0100

    update onListening

--- src/server-utils/on-listening.ts
+++ src/server-utils/on-listening.ts
@@ -2,7 +2,7 @@ import {AddressInfo} from 'net';
 
 export function onListening(log: Function, address: AddressInfo | string | number) {
   return () => {
-    const bind = (typeof address === 'string' || typeof address === 'number') ? `port ${address}` : `pipe ${address.port}`;
+    const bind = (typeof address === 'string' || typeof address === 'number') ? `port ${address}` : `port ${address.port}`;
     log(`Listening on ${bind}`);
   };
 }
