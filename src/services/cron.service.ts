2ae13c57842e9e6ee124f45702753c630ed21ec7
ChrisSensebe <christophe.sensebe@gmail.com>
2019-04-01 13:55:47 +0200

    saving chron state in db and returning a promise

--- src/services/cron.service.ts
+++ src/services/cron.service.ts
@@ -2,10 +2,11 @@ import {LightService} from './lightService';
 import * as cron from 'node-cron';
 import {ScheduledTask} from 'node-cron';
 import {IScheduledState} from '../models/scheduledState.interface';
+import {ScheduledInterface} from '../schemas/scheduledStateSchema';
 
 export class CronService {
 
-  private tasks: ScheduledTask[] = [];
+  private tasks: Map<string, ScheduledTask> = new Map<string, ScheduledTask>();
 
   constructor(private readonly lightService: LightService) {
     this.lightService = lightService;
@@ -15,12 +16,19 @@ export class CronService {
   }
 
   scheduleLightState(scheduledState: IScheduledState) {
-    const lightService = this.lightService;
-    const task = cron.schedule(scheduledState.cronExpression, function () {
-      lightService.setLightState(scheduledState.lightId, scheduledState.state);
-    });
-    task.start();
-    this.tasks = this.tasks.concat(task);
+    const stateSchema = new ScheduledInterface(scheduledState);
+    return stateSchema
+      .save()
+      .then(saved => {
+        const id = saved._id.toString();
+        const lightService = this.lightService;
+        const task = cron.schedule(scheduledState.cronExpression, function () {
+          lightService.setLightState(scheduledState.lightId, scheduledState.state);
+        });
+        this.tasks.set(id, task);
+        task.start();
+        return id;
+      });
   }
 
   getTask(taskId: string): IScheduledState|undefined {
@@ -36,7 +44,7 @@ export class CronService {
 
   clearTasks() {
     this.tasks.forEach(this.destroyTask.bind(this));
-    this.tasks = [];
+    this.tasks.clear();
   }
 
   stopTasks() {
