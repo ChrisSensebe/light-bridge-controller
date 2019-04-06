5e1056a8a4ec7a1b988def8195dedeb519c1e11c
ChrisSensebe <christophe.sensebe@gmail.com>
2019-04-06 23:41:58 +0200

    finishing cronService and updating cron.controller

--- src/controllers/cron.controller.ts
+++ src/controllers/cron.controller.ts
@@ -8,51 +8,41 @@ export class CronController {
 
   scheduleLightState(req: Request, res: Response) {
     const scheduledState = req.body as IScheduledState;
-    this.cronService
-      .scheduleLightState(scheduledState)
-      .then(id => {
-        const location = `/tasks/${id}`;
-        res.set('Location', location);
-        res.status(201).end();
-      });
+    this.cronService.scheduleLightState(scheduledState).then(id => {
+      const location = `/tasks/${id}`;
+      res.set('Location', location);
+      res.status(201).end();
+    });
   }
 
   toggleTask(req: Request, res: Response) {
     const taskId = req.params.id;
-    this.cronService.toggleTask(taskId);
-    res.status(204).end();
+    this.cronService.toggleTask(taskId).then(() => res.status(204).end());
   }
 
   destroyTask(req: Request, res: Response) {
     const taskId = req.params.id;
-    this.cronService.destroyTask(taskId);
-    res.status(200).end();
+    this.cronService.destroyTask(taskId).then(() => res.status(200).end());
   }
 
   getTask(req: Request, res: Response) {
     const taskId = req.params.id;
-    this.cronService
-      .getTask(taskId)
-      .then(task => res.json({task}).status(200).end());
+    this.cronService.getTask(taskId).then(task => res.status(200).send({task}));
   }
 
   getTasks(req: Request, res: Response) {
-    const tasks = this.cronService.getTasks();
-    res.status(200).send({tasks});
+    this.cronService.getTasks().then(tasks => res.status(200).send({tasks}));
   }
 
   clearTasks(req: Request, res: Response) {
-    this.cronService.clearTasks();
-    res.status(204).end();
+    this.cronService.clearTasks().then(() => res.status(204).end());
   }
 
   stopTasks(req: Request, res: Response) {
-    this.cronService.stopTasks();
-    res.status(204).end();
+    this.cronService.stopTasks().then(() => res.status(204).end());
   }
 
   startTasks(req: Request, res: Response) {
-    this.cronService.startTasks();
-    res.status(204).end();
+    this.cronService.startTasks().then(() => res.status(204).end());
   }
 }
--- src/services/cron.service.ts
+++ src/services/cron.service.ts
@@ -16,17 +16,7 @@ export class CronService {
   restoreTasksFromDb() {
     this.tasks = new Map();
     logger.info('restoring tasks...');
-    ScheduledInterface.find()
-      .then(scheduledTasks => scheduledTasks.forEach(scheduledTask => {
-        const lightId = scheduledTask.lightId;
-        const cronExpression = scheduledTask.cronExpression;
-        const state = scheduledTask.state;
-        const started = scheduledTask.started;
-        const scheduledState = {lightId, cronExpression, state, started};
-        const task = this.scheduleTask(scheduledState);
-        logger.info(`light: ${lightId}, cron: ${cronExpression}, on: ${started}, state: ${JSON.stringify(state)}`);
-        this.setTask(task, scheduledTask._id.toString(), scheduledTask.started);
-      }));
+    ScheduledInterface.find().then(scheduledTasks => scheduledTasks.forEach(this.restoreTask.bind(this)));
   }
 
   scheduleLightState(scheduledState: IScheduledState): Promise<string> {
@@ -52,28 +42,42 @@ export class CronService {
       });
   }
 
-  toggleTask(taskId: string) {
+  toggleTask(taskId: string): Promise<void> {
+    return this.getTask(taskId)
+      .then(scheduledTask => {
+        let task = this.tasks.get(taskId);
+        if (!task) {
+          task = this.restoreTask(scheduledTask);
+        }
+        scheduledTask.started ? task.stop() : task.start();
+      });
+  }
+
+  destroyTask(taskId: string): Promise<void> {
+    return ScheduledInterface.deleteOne({_id: taskId}).then(() => this.destroyLocalTask(taskId));
+  }
+
+  clearTasks(): Promise<void> {
+    return ScheduledInterface.remove({}).then(() => {
+      [...this.tasks.keys()].map(this.destroyLocalTask.bind(this));
+    });
+  }
+
+  stopTasks(): Promise<void> {
+    return ScheduledInterface.find().then(() => {
+      this.tasks.forEach(task => task.stop());
+    });
+  }
+
+  startTasks(): Promise<void> {
+    return ScheduledInterface.find().then(() => {
+      this.tasks.forEach(task => task.start());
+    });
 
   }
 
-  destroyTask(taskId: string) {
-  }
-
-  clearTasks() {
-    this.tasks.forEach(this.destroyTask.bind(this));
-    this.tasks.clear();
-  }
-
-  stopTasks() {
-    this.tasks.forEach(task => task.stop);
-  }
-
-  startTasks() {
-    this.tasks.forEach(task => task.start);
-  }
-
-  getTasks(): IScheduledState[] {
-    return [];
+  getTasks(): Promise<IScheduledState[]> {
+    return ScheduledInterface.find().then(scheduledTasks => scheduledTasks);
   }
 
   private scheduleTask(scheduledState: IScheduledState): ScheduledTask {
@@ -81,13 +85,33 @@ export class CronService {
     return cron.schedule(scheduledState.cronExpression, function () {
       const lightId = scheduledState.lightId;
       const state = scheduledState.state;
-      logger.info(`light: ${lightId}; state: ${JSON.stringify(state)}`);
-      lightService.setLightState(lightId, state);
+      lightService.setLightState(lightId, state).then(() => {
+        logger.info(`light: ${lightId}; state: ${JSON.stringify(state)}`);
+      });
     });
   }
 
   private setTask(task: ScheduledTask, id: string, started: boolean) {
     started ? task.start() : task.stop();
     this.tasks.set(id, task);
+  }
+
+  private restoreTask(scheduledTask: any): ScheduledTask {
+    const lightId = scheduledTask.lightId;
+    const cronExpression = scheduledTask.cronExpression;
+    const state = scheduledTask.state;
+    const started = scheduledTask.started;
+    const task = this.scheduleTask({lightId, cronExpression, state, started});
+    logger.info(`light: ${lightId}, cron: ${cronExpression}, on: ${started}, state: ${JSON.stringify(state)}`);
+    this.setTask(task, scheduledTask._id.toString(), scheduledTask.started);
+    return task;
+  }
+
+  private destroyLocalTask(taskId: string) {
+    const task = this.tasks.get(taskId);
+    if (task) {
+      task.destroy();
+    }
+    this.tasks.delete(taskId);
   }
 }
